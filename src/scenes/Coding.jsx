fd44566f3aa36672b3e205366dbd52a69a8c5f68
Shawn <shawnkrise@gmail.com>
2023-03-01 15:56:31 -0700

    commit

--- src/scenes/Coding.jsx
+++ src/scenes/Coding.jsx
@@ -128,8 +128,59 @@ const Coding = () => {
             Similar to HTML, I have utilized CSS for web browser sites such as this.
           </p>
         </motion.div>
-      </div>
 
+        <motion.div
+          className="mx-auto relative bg-deep-greyblue max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person7"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="text-center py-6 text-xl">
+            I have used google Firebase for user Authentication and Database purposes.
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="mx-auto relative bg-deep-greyblue max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person8"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="text-center py-12 text-xl">
+            I have used Sass for CSS styling purposes for projects. 
+          </p>
+        </motion.div>
+
+        <motion.div
+          className="mx-auto relative bg-deep-greyblue max-w-[400px] h-[350px] flex flex-col justify-end p-16 mt-48
+            before:absolute before:top-[-120px] before:-ml-[110px] before:left-1/2 before:content-person9"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ delay: 0.4, duration: 0.6 }}
+          variants={{
+            hidden: { opacity: 0, scale: 0.8 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+        >
+          <p className="text-center py-12 text-xl">
+            I have used Yarn as a JavaScript package manager for some of my projects.
+          </p>
+        </motion.div>
+      </div>
+  {/* CODING PROJECTS */}
       <motion.div
         className="pt-16 pb-5 md:w-2/5 mx-auto text-center"
         initial="hidden"
@@ -164,7 +215,7 @@ const Coding = () => {
       </motion.div>
 
       <div className="flex flex-col md:flex-row md:flex-wrap md:justify-center mt-16 gap-32">
-        {/* PORTFOLIO PROJECT */}
+  {/* PORTFOLIO PROJECT */}
         <motion.div
           className="md:w-1/3 mt-10"
           initial="hidden"
@@ -186,10 +237,21 @@ const Coding = () => {
             <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5 text-justify">
-          Built a custom personal portfolio website using tools such as Reactjs, Tailwind, JavaScript, Framer Motion, HTML, and CSS. You're looking at it right now!
+          Built a custom personal portfolio website using tools such as Reactjs, Tailwind, JavaScript, Framer Motion, HTML, and CSS.
           </p>
+          <div className="flex justify-center my-10 gap-7">
+          <a
+        href="https://github.com/ShawnKrise/shawnkrise.github.io"
+        target="_blank"
+        rel="noreferrer"
+      >
+         <button className="bg-blue text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-blue hover:text-white transition duration-500" >Repository</button>
+      </a>
+          </div>
         </motion.div>
-          {/* TICTACTOE PROJECT */}
+
+  {/* TICTACTOE PROJECT */}
         <motion.div
           className="md:w-1/3 mt-10"
           initial="hidden"
@@ -201,18 +263,39 @@ const Coding = () => {
             visible: { opacity: 1, y: 0 },
           }}
         >
-          <div className="relative h-32">
+          <div className="display: grid relative h-32">
             <div className="z-10">
               <p className="font-Roboto font-semibold text-5xl">02</p>
               <p className="font-Roboto font-semibold text-3xl mt-3">
                 TicTacToe Game
               </p>
             </div>
+            <div className="absolute right-1 top-1 max-w-[120px] max-h-[120px]">
+            <img src="./assets/TicTacToe.png" alt=""/>
+            </div>
             <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5 text-justify">
-          Built a TicTacToe game using Reactjs, JavaScript, CSS, and HTML in order to improve my logic skills using If Else statements. <a className="text-blue" href="https://shawnkrise.github.io/TicTacToeGame/">Click here to check it out!</a>
+          Built a TicTacToe game using Reactjs, JavaScript, CSS, and HTML in order to improve my logic skills using If Else statements. 
           </p>
+          <div className="flex justify-center my-10 gap-7">
+          <a
+        href="https://shawnkrise.github.io/TicTacToeGame/"
+        target="_blank"
+        rel="noreferrer"
+      >
+         <button className="bg-yellow text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-yellow hover:text-white transition duration-500" >Demo</button>
+      </a>
+      <a
+        href="https://github.com/ShawnKrise/TicTacToeGame"
+        target="_blank"
+        rel="noreferrer"
+      >
+         <button className="bg-yellow text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-yellow hover:text-white transition duration-500" >Repository</button>
+      </a>
+      </div>
         </motion.div>
 
 {/* TODO LIST PROJECT */}
@@ -227,18 +310,39 @@ const Coding = () => {
             visible: { opacity: 1, y: 0 },
           }}
         >
-          <div className="relative h-32">
+          <div className="display: grid relative h-32">
             <div className="z-10">
               <p className="font-Roboto font-semibold text-5xl">03</p>
               <p className="font-Roboto font-semibold text-3xl mt-3">
                 Todo List Application
               </p>
-            </div>
+              </div>
+              <div className="absolute right-1 top-1 max-w-[120px] max-h-[120px]">
+            <img src="./assets/TodoList.png" alt=""/>
+              </div>
             <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5 text-justify">
-          Built a simple Todo List application using Reactjs, JavaScript, CSS, and HTML to improve understanding of React hooks usage. <a className="text-blue" href="https://shawnkrise.github.io/TodoList/">Click here to check it out!</a>
+          Built a simple Todo List application using Reactjs, JavaScript, CSS, and HTML to improve understanding of React hooks usage.
           </p>
+          <div className="flex justify-center my-10 gap-7">
+          <a
+        href="https://shawnkrise.github.io/TodoList/"
+        target="_blank"
+        rel="noreferrer"
+      >
+         <button className="bg-yellow text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-yellow hover:text-white transition duration-500" >Demo</button>
+      </a>
+      <a
+        href="https://github.com/ShawnKrise/TodoList"
+        target="_blank"
+        rel="noreferrer"
+      >
+         <button className="bg-yellow text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-yellow hover:text-white transition duration-500" >Repository</button>
+      </a>
+      </div>
         </motion.div>
 
 {/* WORDLE PROJECT */}
@@ -253,18 +357,86 @@ const Coding = () => {
             visible: { opacity: 1, y: 0 },
           }}
         >
-          <div className="relative h-32">
+          <div className="display: grid relative h-32">
             <div className="z-10">
               <p className="font-Roboto font-semibold text-5xl">04</p>
               <p className="font-Roboto font-semibold text-3xl mt-3">
                 Wordle Project
               </p>
             </div>
+            <div className="absolute right-1 top-1 max-w-[120px] max-h-[120px]">
+            <img src="./assets/Wordle.png" alt=""/>
+            </div>
             <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5 text-justify">
-          Built a Wordle clone using Reactjs, JavaScript, CSS, and HTML to gain a better understanding of array inputs in a matrix. <a className="text-blue" href="https://shawnkrise.github.io/WordleClone/">Click here to check it out!</a>
+          Built a Wordle clone using Reactjs, JavaScript, CSS, and HTML to gain a better understanding of array inputs in a matrix. 
           </p>
+          <div className="flex justify-center my-10 gap-7">
+          <a
+        href="https://shawnkrise.github.io/WordleClone/"
+        target="_blank"
+        rel="noreferrer"
+      >
+         <button className="bg-blue text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-blue hover:text-white transition duration-500" >Demo</button>
+      </a>
+      <a
+        href="https://github.com/ShawnKrise/WordleClone"
+        target="_blank"
+        rel="noreferrer"
+      >
+         <button className="bg-blue text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-blue hover:text-white transition duration-500" >Repository</button>
+      </a>
+      </div>
+        </motion.div>
+
+{/* CHAT APP WEBSITE PROJECT */}
+<motion.div
+          className="md:w-1/3 mt-10"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, y: 50 },
+            visible: { opacity: 1, y: 0 },
+          }}
+        >
+          <div className="display: grid relative h-32">
+            <div className="z-10">
+              <p className="font-Roboto font-semibold text-5xl">05</p>
+              <p className="font-Roboto font-semibold text-3xl mt-3">
+                Chat App Website
+              </p>
+            </div>
+            <div className="absolute right-1 top-1 max-w-[120px] max-h-[120px]">
+            <img src="./assets/ChatApp.png" alt=""/>
+            </div>
+            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+          </div>
+          <p className="mt-5 text-justify">
+          Built a fully functional Chat Application using Reactjs, JavaScript, Scss, Yarn, and Google Firebase.
+          </p>
+          <div className="flex justify-center my-10 gap-7">
+          <a
+        href="https://shawnkrise.github.io/ChatApp/#/login"
+        target="_blank"
+        rel="noreferrer"
+      >
+         <button className="bg-blue text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-blue hover:text-white transition duration-500" >Demo</button>
+      </a>
+      <a
+        href="https://github.com/ShawnKrise/ChatApp"
+        target="_blank"
+        rel="noreferrer"
+      >
+         <button className="bg-blue text-deep-blue rounded-sm py-3 px-7 font-bold
+              hover:bg-blue hover:text-white transition duration-500" >Repository</button>
+      </a>
+      </div>
         </motion.div>
 
 {/* COMPANY WEBSITE PROJECT */}
@@ -279,14 +451,14 @@ const Coding = () => {
             visible: { opacity: 1, y: 0 },
           }}
         >
-          <div className="relative h-32">
+          <div className="display: grid relative h-32">
             <div className="z-10">
-              <p className="font-Roboto font-semibold text-5xl">05</p>
+              <p className="font-Roboto font-semibold text-5xl">06</p>
               <p className="font-Roboto font-semibold text-3xl mt-3">
                 Commercial Brokerage Website
               </p>
             </div>
-            <div className="w-1/2 md:w-3/4 h-32 bg-blue absolute right-0 top-0 z-[-1]" />
+            <div className="w-1/2 md:w-3/4 h-32 bg-yellow absolute right-0 top-0 z-[-1]" />
           </div>
           <p className="mt-5 text-justify">
           Built a custom website for Commercial Brokerage LLC using tools such as Reactjs, Tailwind, JavaScript, Framer Motion, HTML, and CSS.
